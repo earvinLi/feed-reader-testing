1ba861b77222f1d46b401bd22f3e283b78ef3ece
Earvin Li <earvin.tli@gmail.com>
2017-11-27 13:18:40 -0800

    add test suite - initial entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,6 +52,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "The menu" */
+
     describe('The menu', function() {
 
         var pageBody = $('body');
@@ -84,12 +85,16 @@ $(function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+    describe('Initial Entries', function() {
+
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
+
+     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
