a939cceb79f9fe9c19480c6c6bcb340ab5408040
Earvin Li <earvin.tli@gmail.com>
2017-11-27 13:19:59 -0800

    before testing load a feed first

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -87,12 +87,16 @@ $(function() {
 
     describe('Initial Entries', function() {
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+      beforeEach(function(done) {
+          loadFeed(0, done);
+      });
+
+      /* TODO: Write a test that ensures when the loadFeed
+       * function is called and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       * Remember, loadFeed() is asynchronous so this test will require
+       * the use of Jasmine's beforeEach and asynchronous done() function.
+       */
 
      });
 
